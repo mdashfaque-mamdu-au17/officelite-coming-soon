8e29d3cdda5d8b3ac141a88348e74fa6e2f7270b
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-01-07 19:46:25 +0530

    common styles reused refactoring

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,20 +4,25 @@ import classNames from 'classnames';
 
 const Button = (props) => {
   const { btnType, applyShadow, styles } = props;
+
+  const applyCommonStyle = () => {
+    return 'rounded-[28px] w-full font-bold text-base leading-7';
+  };
   const applyStyle = () => {
     if (btnType === 'blue') {
-      return 'h-14 rounded-[28px] bg-primary-blue w-full text-primary-white font-bold text-base leading-7 hover:bg-secondary-blue transition delay-200 ease-in-out hover:transition hover:delay-200 hover:ease-in-out';
+      return 'h-14 bg-primary-blue text-primary-white hover:bg-secondary-blue transition delay-200 ease-in-out hover:transition hover:delay-200 hover:ease-in-out';
     }
     if (btnType === 'light-blue') {
-      return 'h-14 rounded-[28px] bg-light-blue w-full text-primary-blue  font-bold text-base leading-7 hover:bg-light-blue-200 transition delay-200 ease-in-out hover:transition hover:delay-200 hover:ease-in-out';
+      return 'h-14 rounded-[28px] bg-light-blue text-primary-blue hover:bg-light-blue-200 transition delay-200 ease-in-out hover:transition hover:delay-200 hover:ease-in-out';
     }
     if (btnType === 'white') {
-      return 'h-14 rounded-[28px] bg-primary-white w-full text-secondary-blue font-bold text-base leading-7';
+      return 'h-14  bg-primary-white text-secondary-blue';
     }
   };
   return (
     <button
       className={classNames(
+        applyCommonStyle(),
         applyStyle(),
         applyShadow && 'shadow-shadow-blue-btn',
         styles
