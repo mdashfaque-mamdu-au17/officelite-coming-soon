aa5a0ee51c5cb42853fd7b02e3896679a66e69c7
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-01-07 19:24:42 +0530

    button completed for all styles

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+const Button = (props) => {
+  const { btnType, applyShadow, styles } = props;
+  const applyStyle = () => {
+    if (btnType === 'blue') {
+      return 'h-14 rounded-[28px] bg-primary-blue w-full text-primary-white font-bold text-base leading-7 hover:bg-secondary-blue transition delay-200 ease-in-out hover:transition hover:delay-200 hover:ease-in-out';
+    }
+    if (btnType === 'light-blue') {
+      return 'h-14 rounded-[28px] bg-light-blue w-full text-primary-blue  font-bold text-base leading-7 hover:bg-light-blue-200 transition delay-200 ease-in-out hover:transition hover:delay-200 hover:ease-in-out';
+    }
+    if (btnType === 'white') {
+      return 'h-14 rounded-[28px] bg-primary-white w-full text-secondary-blue font-bold text-base leading-7';
+    }
+  };
+  return (
+    <button
+      className={classNames(
+        applyStyle(),
+        applyShadow && 'shadow-shadow-blue-btn',
+        styles
+      )}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  btnType: PropTypes.oneOf(['blue', 'light-blue', 'white']).isRequired,
+  applyShadow: PropTypes.bool,
+};
+
+export default Button;
