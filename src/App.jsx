cf46ab5044a218737ac5b260670567d0b7b807de
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-01-08 14:49:25 +0530

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Button from './components/Button/Button';
+import Heading from './components/Texts/Heading';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -19,6 +20,30 @@ function App() {
         <div className="w-[171px] mx-auto pt-3">
           <Button btnType="white">Try for Free</Button>
         </div>
+      </div>
+
+      {/* main heading */}
+      <div className="text-center mt-20">
+        <Heading type="main-heading">
+          A simple solution to complex tasks is coming soon
+        </Heading>
+      </div>
+
+      {/* Heading on form page */}
+      <div className="text-center mt-20">
+        <Heading type="main-heading">Work smarter. Save time.</Heading>
+      </div>
+
+      <div className="text-center mt-10">
+        <Heading type="price-label" styles="text-primary-dark">
+          Free
+        </Heading>
+      </div>
+
+      <div className="text-center mt-10">
+        <Heading type="secondary-heading" styles="text-primary-dark">
+          Basic
+        </Heading>
       </div>
     </div>
   );
