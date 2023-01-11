acfcf280152a55a5532d1dd4edbf8e0a59c14167
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-01-11 13:24:59 +0530

    Hero section implemented

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Hero from '../components/Sections/Hero';
 import Navbar from '../components/Sections/Navbar';
 const Home = () => {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 };
