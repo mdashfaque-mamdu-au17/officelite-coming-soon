e8767edb199ba2ab19ff1f54129d7d4b216bca6f
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-01-11 17:00:39 +0530

    footer progress

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import Footer from '../components/Sections/Footer';
 import Hero from '../components/Sections/Hero';
 import Navbar from '../components/Sections/Navbar';
 const Home = () => {
   return (
-    <>
+    <section>
       <Navbar />
       <Hero />
-    </>
+      <Footer />
+    </section>
   );
 };
 
