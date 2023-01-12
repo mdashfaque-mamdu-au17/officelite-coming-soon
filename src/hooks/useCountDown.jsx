b97f2d63e1db7bc9692d6b6897c83f9b105c3769
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-01-12 15:51:18 +0530

    custom component for count down time

---
+++ src/hooks/useCountDown.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+
+const useCountDown = () => {
+  const [expiryTime, setExpiryTime] = useState('5 mar 2023 15:30:25');
+  const [countdownTime, setCountdownTime] = useState({
+    countdownDays: '',
+    countdownHours: '',
+    countdownMinutes: '',
+    countdownSeconds: '',
+  });
+
+  const countdownTimer = () => {
+    const timeInterval = setInterval(() => {
+      const countdownDateTime = new Date(expiryTime).getTime();
+      const currentTime = new Date().getTime();
+      const remainingDayTime = countdownDateTime - currentTime;
+      const totalDays = Math.floor(remainingDayTime / (1000 * 60 * 60 * 24));
+      const totalHours = Math.floor(
+        (remainingDayTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const totalMinutes = Math.floor(
+        (remainingDayTime % (1000 * 60 * 60)) / (1000 * 60)
+      );
+      const totalSeconds = Math.floor((remainingDayTime % (1000 * 60)) / 1000);
+
+      const runningCountdownTime = {
+        countdownDays: totalDays,
+        countdownHours: totalHours,
+        countdownMinutes: totalMinutes,
+        countdownSeconds: totalSeconds,
+      };
+
+      setCountdownTime(runningCountdownTime);
+
+      if (remainingDayTime < 0) {
+        clearInterval(timeInterval);
+        setExpiryTime(false);
+      }
+    }, 1000);
+  };
+
+  useEffect(() => {
+    countdownTimer();
+  });
+
+  return { countdownTime, expiryTime };
+};
+
+export default useCountDown;
