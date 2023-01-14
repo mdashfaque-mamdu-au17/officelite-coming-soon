394b411b647eb9f2ebca6fa66a446a842162a27e
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-01-14 14:14:51 +0530

    navbar on signup page completed

---
+++ src/components/Sections/NavbarSignUp.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Logo from '../../assets/shared/logo.svg';
+import BGPatters from '../../assets/home/bg-pattern-header.svg';
+
+const NavbarSignUp = () => {
+  return (
+    <nav className="max-w-[1110px] mx-auto">
+      <div className="relative z-20 pt-[46px] flex justify-center items-center sm:justify-start sm:px-10 xl:px-0 sm:pt-[72px] lg:pt-[81px]">
+        <a href="" className="pointer-cursor">
+          <img src={Logo} alt="" />
+        </a>
+      </div>
+      <img
+        src={BGPatters}
+        alt=""
+        className="absolute -z-0 w-[458px] -top-[170px] left-1/2 transform -translate-x-1/2 object-cover sm:hidden"
+      />
+    </nav>
+  );
+};
+
+export default NavbarSignUp;
