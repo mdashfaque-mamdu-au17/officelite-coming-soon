bfd57ceffee3d97df6069d3f3ad82b278c992200
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-01-14 14:13:09 +0530

    count down timer on signup page completed

---
+++ src/components/Sections/CountDown.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import TimeCard from '../Card/TimeCard';
+import useCountDown from '../../hooks/useCountDown';
+const CountDown = () => {
+  const { countdownTime, expiryTime } = useCountDown();
+  return (
+    <div className="flex flex-col items-center lg:flex-row lg:justify-between ">
+      <div className="">
+        <div className="text-center lg:text-left">
+          <h5 className="text-primary-dark uppercase text-base font-bold leading-[48px] tracking-[5px]">
+            Coming <span className="text-primary-blue">5 Mar 2023</span>
+          </h5>
+        </div>
+
+        <div className="flex gap-[13px] pt-[18px] sm:pt-2">
+          <TimeCard
+            theme={false}
+            time={String(countdownTime.countdownDays)}
+            unit="days"
+          />
+          <TimeCard
+            theme={false}
+            time={String(countdownTime.countdownHours)}
+            unit="hours"
+          />
+          <TimeCard
+            theme={false}
+            time={String(countdownTime.countdownMinutes)}
+            unit="mins"
+          />
+          <TimeCard
+            theme={false}
+            time={String(countdownTime.countdownSeconds)}
+            unit="secs"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CountDown;
