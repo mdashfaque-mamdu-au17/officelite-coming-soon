1ff7f7b7ad24bc468693551a9f8360c7c44a1e36
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-01-19 21:11:43 +0530

    fallback page for signup page

---
+++ src/pages/SignUpFallBack.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUpFallBack = () => {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <h1 className="font-bold text-3xl text-primary-blue">Loading...</h1>
+    </div>
+  );
+};
+
+export default SignUpFallBack;
