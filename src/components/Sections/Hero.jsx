e6ae44a241ed11f83426264a945bdc336bfc8c9d
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-01-19 21:10:33 +0530

    navigating to signup page on clicking of button

--- src/components/Sections/Hero.jsx
+++ src/components/Sections/Hero.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Heading from '../Texts/Heading';
 import Paragraph from '../Texts/Paragraph';
 import Button from '../Button/Button';
 import Illustration from '../../assets/home/illustration-charts.svg';
 
 const Hero = () => {
+  const navigate = useNavigate();
+  const navigateToSignupPage = () => {
+    navigate('/signup');
+  };
   return (
     <section className="max-w-[1110px] mx-auto flex flex-col items-center pt-20 sm:flex-row sm:justify-start sm:gap-[63px] md:gap-[95px] sm:px-10 sm:pt-[101px] sm:pb-[140px] xl:px-0 relative z-20 lg:pb-[184px]">
       <div className="w-[171px] h-[192px] shrink-0 sm:order-1 md:w-[281px] md:h-[314px] xl:w-[475px] xl:h-[531px]">
@@ -14,6 +19,7 @@ const Hero = () => {
           className="w-[171px] h-[192px] md:w-[281px] md:h-[314px] xl:w-[475px] xl:h-[531px]"
         />
       </div>
+
       <div className="px-6 text-center pt-[64px] pb-[100px] sm:text-left sm:px-0 sm:max-w-[345px] sm:pt-0 sm:pb-0 md:max-w-[440px] lg:max-w-[540px]">
         <div>
           <Heading type="main-heading">
@@ -28,7 +34,11 @@ const Hero = () => {
           </Paragraph>
         </div>
         <div className="w-[171px] mx-auto sm:mx-0">
-          <Button btnType="blue" applyShadow={true}>
+          <Button
+            btnType="blue"
+            applyShadow={true}
+            onClick={navigateToSignupPage}
+          >
             Get Started
           </Button>
         </div>
