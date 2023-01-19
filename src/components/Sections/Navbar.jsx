9669de6e7532cc9d5017b51809d1d0b3d1432d0d
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-01-19 21:10:45 +0530

    navigating to home page

--- src/components/Sections/Navbar.jsx
+++ src/components/Sections/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Logo from '../../assets/shared/logo.svg';
 import BGPatters from '../../assets/home/bg-pattern-header.svg';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="max-w-[1110px] mx-auto">
       <div className="relative z-20 pt-[46px] flex justify-center items-center sm:pt-[72px] lg:pt-[81px] sm:justify-start sm:px-10 xl:px-0">
-        <a href="" className="pointer-cursor">
+        <Link to="/" className="pointer-cursor">
           <img src={Logo} alt="" />
-        </a>
+        </Link>
       </div>
       <img
         src={BGPatters}
