30ebc6ff397f60486293e9b354dd29412430a4fc
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-01-27 10:11:36 +0530

    transition changed to ease linear

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -10,10 +10,10 @@ const Button = (props) => {
   };
   const applyStyle = () => {
     if (btnType === 'blue') {
-      return 'h-14 bg-primary-blue text-primary-white hover:bg-secondary-blue transition delay-200 ease-in-out hover:transition hover:delay-200 hover:ease-in-out';
+      return 'h-14 bg-primary-blue text-primary-white hover:bg-secondary-blue transition delay-200 ease-in-out hover:transition hover:delay-200 hover:ease-linear';
     }
     if (btnType === 'light-blue') {
-      return 'h-14 rounded-[28px] bg-light-blue text-primary-blue hover:bg-light-blue-200 transition delay-200 ease-in-out hover:transition hover:delay-200 hover:ease-in-out';
+      return 'h-14 rounded-[28px] bg-light-blue text-primary-blue hover:bg-light-blue-200 transition delay-200 ease-in-out hover:transition hover:delay-200 hover:ease-linear';
     }
     if (btnType === 'white') {
       return 'h-14  bg-primary-white text-secondary-blue';
