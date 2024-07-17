bcc82ef0101b7775a9fb1ad5459b1d3a2427adca
Ashfaque <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2024-07-17 22:17:50 +0530

    Update CountDown.jsx

--- src/components/Sections/CountDown.jsx
+++ src/components/Sections/CountDown.jsx
@@ -8,7 +8,7 @@ const CountDown = () => {
       <div className="">
         <div className="text-center lg:text-left">
           <h5 className="text-primary-dark uppercase text-base font-bold leading-[48px] tracking-[5px]">
-            Coming <span className="text-primary-blue">5 May 2024</span>
+            Coming <span className="text-primary-blue">10 Dec 2024</span>
           </h5>
         </div>
 
